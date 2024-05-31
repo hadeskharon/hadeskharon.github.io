e824ae01fe6ea36fd63baaeaa367e1c3706bfed9
hadeskharon <158417483+hadeskharon@users.noreply.github.com>
2024-05-31 15:39:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 
 function getNextVid() { 
-        x = ""
-        
- return x = '<script>
+        iframe = '<script>
         var $newIFrame = document.createElement("iframe");
         $newIFrame.setAttribute("src", "https://spankbang.com/3wlb5/embed/");
         $newIFrame.style.width = "300px";
         $newIFrame.style.height = "300px";
         document.body.appendChild($newIFrame);
         </script>'
-         ; 
+        
+ return iframe; 
 }
 
 
