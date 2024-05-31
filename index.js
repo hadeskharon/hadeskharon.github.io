9dc4eb3173e821c6cfea04081c73cb7b3df189a6
hadeskharon <158417483+hadeskharon@users.noreply.github.com>
2024-05-31 13:26:12 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+var $newIFrame = document.createElement("iframe");
 function getNextVid() { 
- x = "https://spankbang.com/3wlb5/embed/"
+        $newIFrame.setAttribute("src", "https://spankbang.com/3wlb5/embed/");
+        $newIFrame.style.width = "300px";
+        $newIFrame.style.height = "300px";
+        document.body.appendChild($newIFrame);
  return x; 
 }
+
+
+        
