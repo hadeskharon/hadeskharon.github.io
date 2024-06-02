e1549a09722ab2b43caa042c0bb662d1e624e818
hadeskharon <158417483+hadeskharon@users.noreply.github.com>
2024-06-01 23:28:11 -0400

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.logging.Level;
-import java.util.logging.Logger;
