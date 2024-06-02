ef8b7a5102968c06320bf2f69b4ac0da347b797a
hadeskharon <158417483+hadeskharon@users.noreply.github.com>
2024-06-01 22:46:32 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+import static JavaManifestFormatter.JavaManifestFormatter.versionFilePath;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
